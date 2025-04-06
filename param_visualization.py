8f575816064553cba9d67d0ec111446bb4531f74
Qi <1143541029@qq.com>
2025-04-06 15:46:48 +0800

    update

--- param_visualization.py
+++ param_visualization.py
@@ -6,6 +6,6 @@ from nets.pspnet import MyNet
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 model = MyNet(num_classes=2, backbone="resnet101conv3x3stem", down_rate=8,
-              aux_branch=False, pretrained=True).train().to(device)
+              aux_branch=False, pretrained=False).train().to(device)
 
 summary(model, (3, 256, 256))
