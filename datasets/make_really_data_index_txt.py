757a13b1921b15f968737e28dbc64a4a88527c45
Qi <1143541029@qq.com>
2025-04-12 20:15:14 +0800

    update

---
+++ datasets/make_really_data_index_txt.py
@@ -0,0 +1,41 @@
+import os
+import random
+
+# 随机种子
+random.seed(0)
+
+image_file_path = r"./really_smoke_with_mask/images"
+save_path = r"./really_smoke_with_mask/images_index/"
+
+train_val_percent = 1
+train_percent = 0.9
+
+image_list = os.listdir(image_file_path)
+num = len(image_list)
+index_list = range(num)
+train_val_num = int(num * train_val_percent)
+train_num = int(train_val_num * train_percent)
+train_val_index_list = random.sample(index_list, train_val_num)
+train_index_list = random.sample(train_val_index_list, train_num)
+
+print("train and val size: ", train_val_num)
+print("train size: ", train_num)
+
+train_val_stream = open(os.path.join(save_path, "train_val.txt"), "w")
+train_stream = open(os.path.join(save_path, "train.txt"), "w")
+val_stream = open(os.path.join(save_path, "val.txt"), "w")
+
+
+for i in index_list:
+    name = image_list[i][:-4] + '\n'
+    if i in train_val_index_list:
+        train_val_stream.write(name)
+        if i in train_index_list:
+            train_stream.write(name)
+        else:
+            val_stream.write(name)
+
+
+train_val_stream.close()
+train_stream.close()
+val_stream.close()
