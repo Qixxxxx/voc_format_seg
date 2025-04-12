df736c40badb1eb68fc296d5a24b4fc525321301
Qi <1143541029@qq.com>
2025-04-12 20:23:22 +0800

    update

--- get_image_result.py
+++ get_image_result.py
@@ -6,10 +6,10 @@ from predict_model import PredictModel
 
 model = PredictModel()
 
-os.makedirs("result", exist_ok=True)
+os.makedirs("results", exist_ok=True)
 img_names = os.listdir("datasets/viewed_images")
 for image_name in img_names:
     img = Image.open(os.path.join("datasets/viewed_images/", image_name))
     r_image = model.get_image(img)
-    r_image.save("result/" + image_name)
+    r_image.save("results/" + image_name)
     print(" done!")
